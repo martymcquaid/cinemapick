ea4f83b22543ffb8c5baaab93265aec555d7fa26
martymcquaid <martymcquaid@users.noreply.github.com>
2025-12-06 11:02:00 +0000

    AI edit: /root/ChitterChat/team-call-connect/TwilioAPIService/chipify_projects/dec66884-07bf-449e-aae5-0fa25bda5b6e/src/pages/MovieDetail.tsx

--- src/pages/MovieDetail.tsx
+++ src/pages/MovieDetail.tsx
@@ -209,6 +209,30 @@ const MovieDetail: React.FC = () => {
         </div>
       </section>
 
+      {/* Trailer Modal */}
+      {showTrailer && movie?.trailer && (
+        <div className="fixed inset-0 bg-black bg-opacity-90 z-50 flex items-center justify-center p-4">
+          <div className="relative w-full max-w-4xl">
+            <button 
+              onClick={() => setShowTrailer(false)}
+              className="absolute -top-12 right-0 text-white hover:text-yellow-400 transition-colors"
+            >
+              <svg className="w-8 h-8" fill="currentColor" viewBox="0 0 20 20">
+                <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"/>
+              </svg>
+            </button>
+            <div className="aspect-w-16 aspect-h-9">
+              <iframe
+                src={movie.trailer}
+                className="w-full h-96 md:h-[500px] rounded-lg"
+                allowFullScreen
+                title={`${movie.title} Trailer`}
+              />
+            </div>
+          </div>
+        </div>
+      )}
+
       {/* Movie Details */}
       <section className="py-16 px-4">
         <div className="container mx-auto">
