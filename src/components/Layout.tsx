78587a4152682d765d3cfd507314875bc92b11b7
martymcquaid <martymcquaid@users.noreply.github.com>
2025-12-06 10:41:11 +0000

    AI edit: /root/ChitterChat/team-call-connect/TwilioAPIService/chipify_projects/dec66884-07bf-449e-aae5-0fa25bda5b6e/src/components/Layout.tsx

---
+++ src/components/Layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface LayoutProps {
+  children: React.ReactNode
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="min-h-screen bg-black">
+      <main>{children}</main>
+    </div>
+  )
+}
+
+export default Layout
