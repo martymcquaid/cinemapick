b9f60f1d7e05aa895efb3ddb0e0607413aaff3b2
martymcquaid <martymcquaid@users.noreply.github.com>
2025-12-06 10:45:33 +0000

    AI edit: /root/ChitterChat/team-call-connect/TwilioAPIService/chipify_projects/dec66884-07bf-449e-aae5-0fa25bda5b6e/src/pages/ComingSoon.tsx

--- src/pages/ComingSoon.tsx
+++ src/pages/ComingSoon.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
-import MovieCard from '../components/MovieCard'
 import Button from '../components/Button'
 import { mockMovies } from '../utils/mockData'
 
